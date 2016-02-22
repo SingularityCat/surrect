0bcb764df4f831ed2ea3cf4192ddd12469bff67b
Elliot Thomas <elliot@voidptr.uk>
2016-02-22 23:45:36 +0000

    Improved lexer/parser considerably.

--- scroll.py
+++ scroll.py
@@ -1,43 +1,97 @@
-from enum import Enum
-
 """
 # Scroll #
 Simple markup language for summon
 
 ## Syntax: ##
-The first character of each line is important.
-:list()
-     - "#"  = Heading marker. Multiple may be present.
-     - ":"  = Rune specifier.
-     - "\n" = Paragraph separator
-     - Anything else: Paragraph line.
+A scroll is composed of a heriachial tree of runes.
+Each node in the tree can have any number of descendents.
 
-A rune specifier may be followed by zero or more indented lines.
-Lines are indented with 4 spaces. This is removed from each line.
-Indented lines have the same syntax as unindented ones.
+
+Any line type can be indented.
+Indentation is done with groups of four spaces.
+After processing any indentation, the first character of a line
+determines what kind of node it forms.
+:list()
+     - "#"  = Heading marker.
+     - ":"  = Rune specifier.
+     - "!"  = Raw line.
+     - "\n" or just whitespace = Separator
+     - Anything else: Normal line.
 
 ## Example: ##
 Quux, nested inside baz, nested inside bar, nested inside foo.
 
 :code()
-     :foo()
-         :bar()
-             :baz()
-                 :quux()
-
-
-Tokens:
+    !:foo()
+    !    :bar()
+    !        :baz()
+    !            :quux()
 
 
 BNF:
-<scroll> ::= <node> | <scroll> <node>
-<node> ::= <paragraph> | <rune> | <heading>
+<scroll> ::= <node> | <scroll> "\\n" <node>
+<node> ::= <rune> | <raw> | <heading> | <text>
 <indent> ::= "" | "    " | <indent> "    "
 
-<heading> ::= <indent> "#" <string> "\n"
-<rune> ::= <indent> ":" <identifier> "(" <string> ")" "\n"
-<paragraph> ::= <indent> <string> "\n" | <indent> <string> "\n" <paragraph>
+<rune> ::= <indent> ":" <identifier> "(" <argstr> ")"
+<raw> ::= <indent> "!" <string>
+<heading> ::= <indent> "#" <string>
+<text> ::= <indent> <string>
+
+<argstr> ::= <quoted string> | <argstr> "," <quoted string>
 """
+
+from enum import Enum
+from string import whitespace
+
+
+# Escape sequences.
+escape_map = {
+    "n": "\n",  # LF (line feed)
+    "r": "\r",  # CR (carriage return)
+    "t": "\t",  # tab (horizontal)
+    "v": "\v",  # tab (vertical)
+    "a": "\a",  # bell
+    "b": "\b",  # backspace
+    "f": "\f",  # FF (form feed)
+    "\"": "\"",
+    "\'": "\'",
+    "\\": "\\",
+}
+
+
+def lex_argstr(argstr):
+    """
+    Turns a string representing a list of strings, into a list of strings.
+    Strings are either "quoted" or 'quoted', and are comma separated.
+    Understands most c/python escape codes within strings.
+    See also: escape_map
+    """
+    argv = []
+    curarg = []
+    # strterm == None means we're not in a quoted string.
+    strterm = None
+    escaped = False
+    for char in argstr:
+        if strterm is None:
+            if char == "\"" or char == "\'":
+                strterm = char
+            elif char == ",":
+                argv.append("".join(curarg))
+                curarg = []
+            # Ignore anything else.
+        elif char == "\\":
+            escaped = True
+        else:
+            if char == strterm and not escaped:
+                strterm = None
+            else:
+                if escaped and char in escape_map:
+                    char = escape_map[char]
+                curarg.append(char)
+    if strterm is not None:
+        argv.append("".join(curarg))
+    return argv
 
 
 def gensplit(src, sep):
@@ -48,6 +102,7 @@ def gensplit(src, sep):
 
 
 def charcount(s, c):
+    """counts occurances of c at the start of s."""
     i = 0
     while i < len(s) and s[i] == c:
         i += 1
@@ -55,13 +110,13 @@ def charcount(s, c):
 
 
 class token(Enum):
-    root = 0
+    rune = 0
     indent = 1
-    heading = 2
-    rune = 3
+    raw = 2
+    heading = 3
     text = 4
     blank = 5
-    
+
 
 def lex(source):
     if isinstance(source, str):
@@ -77,22 +132,28 @@ def lex(source):
             yield (token.indent, None)
 
         # identify line type.
-        if line.startswith("#"):
+        if line.startswith("!"):
+            # Raw lines are unstripped.
+            yield (token.raw, line[1:])
+
+        elif line.startswith("#"):
             level = charcount(line, "#")
-            yield (token.heading, (level, line.strip("# ")))
+            yield token.heading, (level, line.strip("#" + whitespace))
 
         elif line.startswith(":"):
             # Runes have the form:
             # ":" || <rune id> || "(" || args || ")"
             runeid, _, enil = line[1:].partition("(")
             args, _, _, = enil.rpartition(")")
-            yield (token.rune, (runeid, args))
-
-        elif len(line) > 0:
-            yield (token.text, line)
+            yield token.rune, (runeid, lex_argstr(args))
 
         else:
-            yield (token.blank, None)
+            line = line.strip()
+            if len(line) > 0:
+                yield (token.text, line)
+            else:
+                yield (token.blank, None)
+
 
 class ScrollNode:
     def __init__(self, toktype, tokval):
@@ -104,11 +165,11 @@ class ScrollNode:
 def parse(tokens):
     indent = 0
     prev_indent = 0
-    root = ScrollNode(token.root, None)
+    root = ScrollNode(token.rune, ("root", None))
     scope_stack = []
     scope_cur = root
     prev_node = root
-    
+
     for toktype, tokval in tokens:
         # Determine indentation level.
         if toktype == token.indent:
