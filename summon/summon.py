fa179a581bbf58e951fd53c4b373aa6e49c6e3a8
Elliot Thomas <elliot@voidptr.uk>
2016-03-05 01:32:52 +0000

    First working scan_category function.

--- summon/summon.py
+++ summon/summon.py
@@ -1,33 +1,85 @@
-from os import walk, path
-from sys import path
+from os import listdir, path
+from collections import OrderedDict
 from configparser import SafeConfigParser as ConfigParser
 
 from . import rune
 from . import scroll
 from . import tree
-from . import toc
+from . import page
 
-def scan_category(path):
-    catdict = {
-        "name": path.basename(path).title(),
-        "index": None
+
+def scroll_html_pfunc(pth):
+    return path.splitext(pth)[0] + ".html"
+
+
+def scan_category(cpath, pageset, phy_root, pathfunc=scroll_html_pfunc):
+    cfpath = cpath
+    if path.isdir(cpath):
+        cfpath = path.join(cpath, "cat")
+    else:
+        cpath = path.dirname(cpath)
+
+    catcfg = {
+        "name": path.basename(cpath).title(),
+        "index": None,
+        "scan": True,
+        "entries": []
     }
-    entries = []
     exclude = set()
+    catdict = OrderedDict()
 
-    with open(path, "r") as catfile:
-        cd, en, ex = scroll.catparse(scroll.catlex(catfile))
-        catdict.update(cd)
-        entries = en
-        exclude = ex
+    if path.exists(cfpath):
+        with open(cfpath, "r") as catfile:
+            cc, ex = scroll.catparse(scroll.catlex(catfile))
+            catcfg.update(cc)
+            exclude.union(ex)
 
-    tocnode = toc.TocNode(catdict["name"], catdict["index"])
+    if catcfg["scan"]:
+        for p in listdir(cpath):
+            rp = path.join(cpath, p)
+            if path.isdir(rp):
+                sce = scroll.CatEntry("subcat", None, p)
+                catcfg["entries"].append(sce)
+            elif rp.endswith(".scroll"):
+                sce = scroll.CatEntry("page", None, p)
+                catcfg["entries"].append(sce)
 
+    # Deal with explicit items first.
+    for ent in catcfg["entries"]:
+        ename = ent.name
+        inpath = path.join(cpath, ent.path)
+        outpath = pathfunc(inpath)
+
+        if ent.kind == "subcat":
+            cn2, cd2 = scan_category(inpath, pageset, phy_root, pathfunc)
+            if ename is None:
+                ename = cn2
+            catdict[ename] = cd2
+            exclude.add(ent.path)
+        elif ent.kind == "page" and path.exists(inpath):
+            p = page.Page(inpath)
+            p.read_metadata()
+            pageset[outpath] = p
+            # Name from catfile overrides page context
+            if ename is None:
+                if "name" in p.context:
+                    ename = p.context["name"]
+                else:
+                    ename = path.splitext(path.basename(ent.path))[0].title()
+            catdict[ename] = path.relpath(outpath, phy_root)
+            exclude.add(ent.path)
+        elif ent.kind == "link":
+            if ename is None:
+                ename = ent.path
+            catdict[ename] = ent.path
+    return catcfg["name"], catdict
 
 DEFAULT_CONFIG = {
     "summon": {
         "root dir": "root",
-        "rune dir": "runes"
+        "rune dir": "runes",
+        "prefix": "/",
+        "build dir": "build"
     },
     "page": {
         "page order": "header main nav footer"
