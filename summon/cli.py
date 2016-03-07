a61fd51996e4e50fc9662ac185239a257820b6c4
Elliot Thomas <elliot@voidptr.uk>
2016-03-07 01:50:30 +0000

    WIP commandline interface.

---
+++ summon/cli.py
@@ -0,0 +1,56 @@
+from logging import getLogger
+from os import path
+from argparse import ArgumentParser, ArgumentError
+
+from .summon import make_default_config
+
+mode_choices = {"build", "gen"}
+
+arg_parser = ArgumentParser()
+arg_parser.add_argument("-f", "--file", "--summonfile",
+    dest="summonfile", action="store", default="Summonfile",
+    help="Read a file other then Summonfile for configuration."
+)
+arg_parser.add_argument("-n", "--noop",
+    dest="noop", action="store_true", default=False,
+    help="Do a dry run - no files will be written."
+)
+
+arg_parser.add_argument("-m", "--mode",
+    dest="mod", action="store", default="build", choices=mode_choices,
+    help="Set a build mode."
+)
+
+arg_parser.add_argument("-b", "--build",
+    dest="mode", action="store_const", const="gen",
+    help="Switch to build mode, this is the default."
+)
+
+arg_parser.add_argument("-g", "--gen",
+    dest="mode", action="store_const", const="gen",
+    help="Switch to generate mode, creates a default Summonfile."
+)
+
+arg_parser.add_argument("-o", "--force",
+    dest="force", action="store_true", default=False,
+    help="Overwrite an existing Summonfile"
+)
+
+
+def main():
+    args = arg_parser.parse_args()
+    cfg = make_default_config()
+
+    if args.mode == "gen":
+        if args.force or not path.exists(args.summonfile):
+            cfg.write(args.summonfile)
+            return 0
+        else
+            return 1
+
+    cfg.read(args.summonfile)
+    
+
+if __name__ == "__main__":
+    status = main()
+    exit(status)
