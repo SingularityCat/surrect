506c926e7944f2c619632ee7392ba2679814c4df
Elliot Thomas <elliot@voidptr.uk>
2017-01-23 01:00:20 +0000

    Renamed nav to source, major rework. Moved nav generator from (nav|source) to summon. Page abstraction is now a 'source', moved to the source module. Added renderer classes and registry to summon, reformed default config.

---
+++ surrect/source.py
@@ -0,0 +1,176 @@
+from collections import namedtuple, OrderedDict
+from collections.abc import Mapping
+from os import listdir, path
+
+from enum import Enum
+from typing import Tuple, Callable
+
+import html
+
+from . import scroll
+
+
+def read_scroll_metadata(scrpath : str) -> dict:
+    """
+    Read metadata from a scroll file.
+    Metadata exists as series of special comments, each starting with
+    three hashes, optionally interleaved with regular comments.
+    Metadata comments are key-value pairs.
+    Reading stops after the first non-comment token.
+    """
+    metadata = {}
+    with open(scrpath, "r") as source:
+        lexer = scroll.lexer.lex(source)
+        for token, value in lexer:
+            if token is not scroll.lexer.TOKEN_COMMENT:
+                break
+            # One of the hashes is removed by the lexer.
+            elif value.startswith("##"):
+                key, _, value = value[2:].partition(":")
+                metadata[key.strip()] = value.strip()
+        lexer.close()
+    return metadata
+
+
+def category_load(catpath: str) -> dict:
+    """
+    Determines a category configuration.
+    A category config is a dict, containing
+     - name
+     - index : str, physical path to a scroll for use as a category index.
+     - scan : bool, is this category configured to do scanning?
+     - entries : list of (str, str/None, str) named tuples: type, name, path.
+     - exclude : set os str, paths to exclude.
+     - catpath : str, the prefix for all physical paths in this category.
+    Returns said dict.
+    """
+    # Normalise the path.
+    catpath = path.normpath(catpath)
+    # Choose a default name based on the last part of the path.
+    default_name = path.basename(catpath).strip().title()
+    cfpath = catpath
+    if path.isdir(catpath):
+        cfpath = path.join(catpath, "cat")
+    else:
+        catpath = path.dirname(catpath)
+        if catpath == "":
+            catpath = "."
+
+    catcfg = {
+        "name": default_name,
+        "index": None,
+        "scan": True,
+        "entries": [],
+        "exclude": set(),
+        "catpath": catpath
+    }
+
+    if path.exists(cfpath):
+        with open(cfpath, "r") as catfile:
+            cc = scroll.catparse(scroll.catlex(catfile))
+            catcfg.update(cc)
+    return catcfg
+
+
+def category_scan(catcfg: dict) -> None:
+    """
+    Adds all files in a directory to the end of the
+    entries list in a category configuration.
+    """
+    # Construct set of accounted for paths.
+    accounted = {ent.path for ent in catcfg["entries"]} | catcfg["exclude"]
+    catpath = catcfg["catpath"]
+    for p in listdir(catpath):
+        # Skip accounted for paths.
+        if p in accounted:
+            continue
+        rp = path.join(catpath, p)
+        if path.isdir(rp):
+            sce = scroll.CatEntry("subcat", None, p)
+            catcfg["entries"].append(sce)
+        elif path.exists(rp) and rp.endswith(".scroll"):
+            sce = scroll.CatEntry("page", None, p)
+            catcfg["entries"].append(sce)
+
+
+SourceType = Enum("SourceType", ("SCROLL", "RESOURCE", "LINK"))
+class Source:
+    __slots__ = ("kind", "source", "relpath", "ref", "metadata")
+
+    def __init__(self, kind, source, relpath, metadata):
+        self.kind = kind
+        self.source = source
+        self.relpath = relpath
+        self.ref = None
+        self.metadata = metadata
+
+
+def category_build(catroot: str, catpath: str, sources: Mapping) -> Tuple[str, OrderedDict]:
+    if sources is None:
+        sources = {}
+
+    # Get the category configuration.
+    catcfg = category_load(catpath)
+    if catcfg["scan"]:
+        category_scan(catcfg)
+    catpath = catcfg["catpath"]
+    exclude = catcfg["exclude"]
+    # The category dict.
+    catdict = OrderedDict()
+
+    if catcfg["index"] is not None:
+        srcpath = path.abspath(path.join(catpath, catcfg["index"]))
+        relpath = path.relpath(srcpath, start=catroot)
+
+        if path.exists(srcpath):
+            srcent = Source(SourceType.SCROLL, srcpath, relpath, read_scroll_metadata(srcpath))
+            catdict[None] = srcent
+            sources[srcpath] = srcent
+            exclude.add(catcfg["index"])
+
+    for ent in catcfg["entries"]:
+        ename = ent.name
+        # Category and scroll paths are all relative to their directory.
+        srcpath = path.abspath(path.join(catpath, ent.path))
+        relpath = path.relpath(srcpath, start=catroot)
+        if ent.path in exclude:
+            # Nothing to do.
+            continue
+        if srcpath in sources:
+            # Print a warning?
+            continue
+        if ent.kind == "subcat":
+            scn, scd = category_build(catroot, srcpath, sources)
+            if ename is None:
+                ename = scn
+            catdict[ename] = scd
+            if ".." not in scd:
+                # Set parent category reference.
+                scd[".."] = catdict
+        elif ent.kind == "resource":
+            sources[srcpath] = Source(SourceType.RESOURCE, srcpath, relpath, {})
+        elif (ent.kind == "page" or ent.kind == "secret") \
+                and path.exists(srcpath):
+            metadata = read_scroll_metadata(srcpath)
+            srcent = Source(SourceType.SCROLL, srcpath, relpath, metadata)
+            if ename is None:
+                if "name" in metadata:
+                    ename = metadata["name"]
+                else:
+                    ename = path.splitext(path.basename(ent.path))[0].title()
+            if ent.kind != "secret":
+                catdict[ename] = srcent
+            sources[srcpath] = srcent
+        elif ent.kind == "link":
+            if ename is None:
+                ename = ent.path
+            catdict[ename] = ent.path
+        else:
+            # Print a warning?
+            continue
+    return catcfg["name"], catdict
+
+
+def source(rootcat):
+    sources = OrderedDict()
+    return *category_build(rootcat, rootcat, sources), sources
--- surrect/summon.py
+++ surrect/summon.py
@@ -4,136 +4,237 @@ import sys
 import codecs
 
 from os import path
-from configparser import ConfigParser
 from functools import partial
+from shutil import copyfile
 
-from . import nav
+from . import scroll
+from . import rune
+from .source import source, SourceType
+from .util import brace_expand
 
 
 def get_outfunc_msg(colour=sys.stdout.isatty()):
-    u8w = codecs.getwriter("utf8")
-
-    if colour:
-        return partial(print, "\033[35m⛧ \033[0m", flush=True, file=codecs.getwriter("utf8")(sys.stderr.buffer, "replace"))
-    else:
-        return partial(print, "⛧ ")
+    u8f = codecs.getwriter("utf8")(sys.stderr.buffer, "replace")
+    return partial(print, "\033[35m⛧ \033[0m" if colour else "⛧ ", flush=True, file=u8f)
 
 
-def generate_page_builder(cfg, catdict):
-    section_order = [sec.strip() for sec in cfg["page"]["order"].split(" ")]
+def gen_navigation_renderer(wrap_init_func,
+                            wrap_fini_func,
+                            nav_init_func,
+                            nav_fini_func,
+                            ent_init_func,
+                            ent_fini_func,
+                            cat_func,
+                            idxcat_func,
+                            lnk_func,
+                            curlnk_func,
+                            escape_func):
+    def navigation_render(catdat, ctx, catname=None, cursource=None, function_entry=True):
+        ctx = ctx.copy()
+        if function_entry:
+            wrap_init_func(ctx)
+        entrywrap = bool(catname)
+        if entrywrap:
+            ctx["name"] = catname
+            if None in catdat:
+                ctx["ref"] = catdat[None].ref
+                yield idxcat_func(ctx)
+            else:
+                yield cat_func(ctx)
+            yield nav_init_func(ctx)
+        for name, source in catdat.items():
+            if name is None:
+                continue
+            name = escape_func(name, context=ctx)
+            if entrywrap:
+                yield ent_init_func(ctx)
+            # If we encounter a category here, recurse into it.
+            if isinstance(source, dict):
+                yield from navigation_render(source, ctx, name, cursource, function_entry=False)
+            else:
+                ctx["name"] = name
+                ctx["ref"] = source.ref
+                if source is cursource:
+                    yield curlnk_func(ctx)
+                else:
+                    yield lnk_func(ctx)
+            if entrywrap:
+                yield ent_fini_func(ctx)
+        if entrywrap:
+            yield nav_fini_func(ctx)
+        if function_entry:
+            wrap_fini_func(ctx)
 
-    # Convert config things to formatter functions
-    fmtfuncs = {}
-    for fmtname, fmtval in cfg["nav"].items():
-        if fmtname == "category":
-            fmtfuncs["catfunc"] = fmtval.format
-        elif fmtname == "indexed category":
-            fmtfuncs["idxcatfunc"] = fmtval.format
-        elif fmtname == "entry list start":
-            fmtfuncs["entlstinitfunc"] = fmtval.format
-        elif fmtname == "entry list end":
-            fmtfuncs["entlstfinifunc"] = fmtval.format
-        elif fmtname == "entry start":
-            fmtfuncs["entinitfunc"] = fmtval.format
-        elif fmtname == "entry end":
-            fmtfuncs["entfinifunc"] = fmtval.format
-        elif fmtname == "link":
-            fmtfuncs["lnkfunc"] = fmtval.format
-        elif fmtname == "current link":
-            fmtfuncs["curlnkfunc"] = fmtval.format
+    return navigation_render
 
-    nav_render = nav.gen_navigation_renderer(**fmtfuncs)
 
-    def do_header(page, outfile):
-        outfile.write(cfg["header"]["start"])
-        outfile.write(cfg["header"]["format"].format(*page.context))
-        outfile.write(cfg["header"]["end"])
+renderers = {}
 
-    def do_main(page, outfile):
-        outfile.write("<main>")
-        page.read_scroll()
-        for tag in page.build_main():
-            outfile.write(tag)
-        outfile.write("</main>")
 
-    def do_nav(page, outfile):
-        outfile.write(cfg["nav"]["start"])
-        for tag in nav_render(catdict, None, page.linkpath):
-            outfile.write(tag)
-        outfile.write(cfg["nav"]["end"])
+def renderer_register(rendid, rendcls):
+    renderers[rendid] = rendcls
 
-    def do_footer(page, outfile):
-        outfile.write(cfg["footer"]["start"])
-        outfile.write(cfg["footer"]["format"].format(*page.context))
-        outfile.write(cfg["footer"]["end"])
 
-    def page_builder(page, outpath):
-        title = path.splitext(path.basename(page.filepath))[0].title()
-        if "title" in page.context:
-            title = page.context["title"]
+def renderer(rendid):
+    return lambda rendcls: renderer_register(rendid, rendcls)
 
-        outfile = open(outpath, "w")
-        outfile.write("<!DOCTYPE html>\n")
-        outfile.write("<head>")
-        outfile.write("<meta charset=\"UTF-8\"/>")
-        outfile.write("<title>" + title + "</title>")
-        outfile.write(cfg["page"]["head"])
-        outfile.write("</head>")
-        outfile.write("<body>")
-        for sec in section_order:
-            if sec == "header":
-                do_header(page, outfile)
-            elif sec == "main":
-                do_main(page, outfile)
-            elif sec == "nav":
-                do_nav(page, outfile)
-            elif sec == "footer":
-                do_footer(page, outfile)
-        outfile.write("</body>")
 
-    return page_builder
+def renderer_lookup(rendid):
+    return renderers[rendid]
+
+
+class Renderer:
+    def __init__(self, build_dir, root_context, fmt):
+        self.build_dir = build_dir
+        self.context = root_context.copy()
+        self.fmt = fmt
+        self.context["type"] = self.fmt
+
+    def ritual(self, sources):
+        """Prep step"""
+        for srcpath, source in sources.items():
+            source.ref = source.relpath
+
+    def summon(self, sources, catroot):
+        """Output step"""
+        raise NotImplementedError("summon not implemented.")
+
+
+@renderer("site")
+class SiteRenderer(Renderer):
+    def __init__(self, build_dir, root_context, fmt, cfg):
+        super().__init__(build_dir, root_context, fmt)
+        self.load_cfg(cfg)
+
+    def load_cfg(self, cfg):
+        """Load configuration, filling in defaults."""
+        self.base_context.update(cfg.get("context", {}))
+        self.path_fmt = cfg.get("path format", "{dir}/{filebase}.{type}")
+        self.reference_fmt = cfg.get("ref format", self.path_fmt)
+        self.page_comp = cfg.get("page composition", ["main", "nav"])
+        self.running_blocks = {}
+        for name, block in cfg.get("running blocks", {}).items():
+            if isinstance(block, str):
+                self.running_blocks[name] = block
+            else:
+                # If the running block is not a string, join on a newline.
+                self.running_blocks[name] = "\n".join(block)
+        self.nav_renderer = gen_navigation_renderer(
+            cfg["nav"].get("start", ""),
+            cfg["nav"].get("end", ""),
+            cfg["nav"].get("category", "{name}\n"),
+            cfg["nav"].get("indexed category", "{name} ({ref})\n"),
+            cfg["nav"].get("entry list start", ""),
+            cfg["nav"].get("entry list end", ""),
+            cfg["nav"].get("entry start", ""),
+            cfg["nav"].get("entry end", ""),
+            cfg["nav"].get("link", "{name} ({ref})\n"),
+            cfg["nav"].get("current link", "{name} ({ref})\n"),
+            rune.escape_lookup(self.fmt)
+        )
+
+    def ritual(self, sources):
+        for srcpath, source in sources.items():
+            ctx = self.context.copy()
+            ctx["path"] = sources.relpath
+            ctx["dir"] = path.dirname(ctx["path"])
+            ctx["filename"] = path.basename(ctx["path"])
+            ctx["filebase"], ctx["fileext"] = path.splitext(ctx["filename"])
+            ctx.update(source.metadata)
+            source.metadata = ctx
+            source.relpath = self.path_fmt.vformat(source.metadata)
+            source.ref = self.ref_fmt.vformat(source.metadata)
+
+    def summon(self, sources, catroot):
+        for srcpath, source in sources.items():
+            dstpath = path.join(self.build_dir, source.relpath)
+            if source.kind is SourceType.SCROLL:
+                with open(srcpath) as srcfile:
+                    scroll_tree = scroll.parser.parse(scroll.lexer.lex(srcfile))
+
+                rune_tree = rune.assemble(scroll_tree)
+                inscribed_tree = rune.inscribe(rune_tree, self.fmt, source.metadata)
+
+                with open(dstpath) as dstfile:
+                    for name in self.page_comp:
+                        if name == "main":
+                            for node in inscribed_tree:
+                                if type(node.data) is str:
+                                    dstfile.write(node.data)
+                        elif name == "nav":
+                            for fragment in self.nav_renderer(catroot, source.metadata, cursource=source):
+                                dstfile.write(fragment)
+                        elif name in self.running_blocks:
+                            block = self.running_blocks[name]
+                            dstfile.write(block.vformat(source.metadata))
+                        else:
+                            # Print warning?
+                            pass
+            elif source.kind is SourceType.RESOURCE:
+                copyfile(srcpath, dstpath)
+
+
+def load_renderers(cfg, build_dir, root_context):
+    rendmap = {}
+    for rendname, rendcfg in cfg.items():
+        rid, _, fmt = rendcfg["renderer"].partition(":")
+        rendinst = renderer_lookup(rid)(build_dir, root_context, fmt, rendcfg)
+        rendmap[rendname] = rendinst
+    return rendmap
 
 
 DEFAULT_CONFIG = {
     "summon": {
         "root dir": "root",
         "rune dir": "runes",
-        "prefix": "/",
-        "build dir": "build"
+        "build dir": "build",
+        "map": [
+            ["*.man.scroll", "manual"],
+            ["*.scroll", "site"]
+        ],
+        "context": {
+        }
     },
-    "page": {
-        "order": "header main nav footer",
-        "head": "<meta name=\"generator\" content=\"⛧ surrect\"/>"
-    },
-    "nav": {
-        "start": "<nav id=\"navigation\">",
-        "end": "</nav>",
-        "category": nav.CATFMT,
-        "indexed category": nav.IDXCATFMT,
-        "entry list start": nav.ENTLSTINITFMT,
-        "entry list end": nav.ENTLSTFINIFMT,
-        "entry start": nav.ENTINITFMT,
-        "entry end": nav.ENTFINIFMT,
-        "link": nav.LNKFMT,
-        "current link": nav.CURLNKFMT
-    },
-    "header": {
-        "start": "<header>",
-        "end": "</header>",
-        "format": ""
-    },
-    "footer": {
-        "start": "<footer>",
-        "end": "</footer>",
-        "format": ""
+    "renderers": {
+        "manual": {
+            "renderer": "site:man",
+            "path format": "man/{section}/{filebase}.{section}",
+            "ref format": "{filebase}({section})",
+            "page": {
+                "order": "main"
+            }
+        },
+        "site": {
+            "renderer": "site:html",
+            "path format": "{dir}/{filebase}.html",
+            "ref format": "{dir}/{filebase}.html",
+            "page composition": ["head", "main", "nav", "tail"],
+            "nav": {
+                "start": "<nav id=\"navigation\">",
+                "end": "</nav>",
+                "category": "<h1>{name}</h1>",
+                "indexed category": "<a href=\"{ref}\"><h1>{name}</h1></a>",
+                "entry list start": "<ul>",
+                "entry list end": "</ul>",
+                "entry start": "<li>",
+                "entry end": "</li>",
+                "link": "<a href=\"{ref}\">{name}</a>",
+                "current link": "<a class=\"curlnk\" href=\"{ref}\">{name}</a>"
+            },
+            "running blocks": {
+                "head": [
+                    "<!DOCTYPE html>",
+                    "<head>",
+                    "    <meta charset=\"UTF-8\"/>",
+                    "    <title>{title}</title>",
+                    "    <meta name=\"generator\" content=\"⛧ surrect\"/>",
+                    "</head>",
+                    "<body>"
+                ],
+                "tail": [
+                    "</body>"
+                ]
+            }
+        }
     }
 }
-
-
-def make_default_config():
-    cfg = ConfigParser()
-    for sect, opts in DEFAULT_CONFIG.items():
-        cfg.add_section(sect)
-        for opt, val in opts.items():
-            cfg[sect][opt] = val
-    return cfg
